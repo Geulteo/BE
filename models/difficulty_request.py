4949aa9bf879054d6821cde7a1f55f52fbdf5301
chaeelin <chaeee0123@gmail.com>
2025-12-16 17:57:47 +0900

    :recycle: Refactor: Target enum 추가

--- models/difficulty_request.py
+++ models/difficulty_request.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-from core.type_enums import IntentType, TemplateId
+from core.type_enums import IntentType, TemplateId, TargetType
 
 
-class DifficultyDiagnosisRequest(BaseModel):\
+class DifficultyDiagnosisRequest(BaseModel):
     # 난이도 진단 요청 Request
     user_sentence: str
     intent: IntentType
     template_id: TemplateId
-    target: str
+    target: TargetType
