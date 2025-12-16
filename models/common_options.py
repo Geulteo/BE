e0017bc2e9c6ee14737498dc1c65ddfa35844f5c
chaeelin <chaeee0123@gmail.com>
2025-12-16 16:35:03 +0900

    :recycle: Refactor: enum 변경

--- models/common_options.py
+++ models/common_options.py
@@ -3,4 +3,4 @@ from typing import Literal
 # keyword 입력 드롭다운 옵션
 ToneOption = Literal["neutral", "polite", "business", "emotional"]
 LengthOption = Literal["short", "normal", "detailed"]
-TargetOption = Literal["professor", "senior_junior", "friend", "manager"]
+TargetOption = Literal["professor", "senior_junior", "friend", "boss"]
